55fbf8607115797988aa1091279a82e064da14f2
Alison Hooper <designedbycozy@gmail.com>
2022-09-08 07:35:18 -0400

    Initial Commit

---
+++ First Project/First_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  First_ProjectApp.swift
+//  First Project
+//
+//  Created by Alison Hooper on 9/8/22.
+//
+
+import SwiftUI
+
+@main
+struct First_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
